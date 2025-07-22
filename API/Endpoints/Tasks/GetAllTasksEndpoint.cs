3d37788e1d6574be0ec2b433f39ce6e56705db2d
Mario Espinosa <79775251+MarioEspinosaFiguerez@users.noreply.github.com>
2025-07-22 22:03:33 +0200

    First task endpoint (GetAll)

---
+++ API/Endpoints/Tasks/GetAllTasksEndpoint.cs
@@ -0,0 +1,26 @@
+﻿
+namespace API.Endpoints.Tasks;
+
+public class GetAllTasksEndpoint : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        // Assign Multiples routes using the same Handler
+        async Task<IResult> GetAllTasks(ITaskService _taskService)
+        {
+            var tasks = await _taskService.GetAllTasks();
+            return Results.Ok(tasks);
+        }
+
+        app.MapGet("/tasks", GetAllTasks).WithTags("Tasks");
+        app.MapGet("/tasks/getAllTasks", GetAllTasks).WithTags("Tasks");
+
+        /* Using 1 Route for 1 Handler
+        app.MapGet("/tasks", async (ITaskService _taskService) =>
+        {
+            var tasks = await _taskService.GetAllTasks();
+            return Results.Ok(tasks);
+        });
+        */
+    }
+}

0f07693ccce026da67664f48ca3485972320b17f
Mario Espinosa <79775251+MarioEspinosaFiguerez@users.noreply.github.com>
2025-07-22 21:27:12 +0200

    Function that allow us to get all endpoints that implement IEndpoint and map all the endpoints

---
+++ API/Extensions/EndpointExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+
+namespace API.Extensions
+{
+    public static class EndpointExtensions
+    {
+        public static void MapEndpoints(this IEndpointRouteBuilder app)
+        {
+            var endpointType = typeof(IEndpoint);
+
+            var endpoints = Assembly.GetExecutingAssembly().GetTypes()
+                .Where(t => !t.IsAbstract && endpointType.IsAssignableFrom(t))
+                .Select(Activator.CreateInstance)
+                .Cast<IEndpoint>();
+
+            foreach (var endpoint in endpoints)
+            {
+                endpoint.MapEndpoint(app);
+            }
+        }
+    }
+}

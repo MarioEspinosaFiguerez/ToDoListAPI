51c7ae26655a35f748499808c0110f618f879721
Mario Espinosa <79775251+MarioEspinosaFiguerez@users.noreply.github.com>
2025-07-22 22:03:05 +0200

    TaskService

---
+++ Application/Interfaces/ITaskService.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.Interfaces;
+
+public interface ITaskService
+{
+    Task<IEnumerable<ToDoTaskDTO>> GetAllTasks();
+}
---
+++ Application/Services/TaskService.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Application.Services;
+
+public class TaskService : ITaskService
+{
+    public readonly ITaskRepository _taskRepository;
+
+    public TaskService(ITaskRepository taskRepository) => _taskRepository = taskRepository;
+
+    public async Task<IEnumerable<ToDoTaskDTO>> GetAllTasks()
+    {
+        var tasks = await _taskRepository.GetAllTasks();
+
+        return tasks.Select(task => new ToDoTaskDTO
+        {
+            Title = task.Title,
+            Description = task.Description,
+            UserAssignedId = task.UserAssignedId,
+            Priority = task.Priority,
+            State = task.State,
+            Deadline = task.Deadline,
+        });
+    }
+}

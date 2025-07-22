685e86587351d82a68423ff6eaf764053bf01ad5
Mario Espinosa <79775251+MarioEspinosaFiguerez@users.noreply.github.com>
2025-07-22 22:02:55 +0200

    Add ToDoTaskDTO Validator with FluentValidation

---
+++ Application/Validators/ToDoTaskDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿namespace Application.Validators;
+
+public class ToDoTaskDtoValidator : AbstractValidator<ToDoTaskDTO>
+{
+    public ToDoTaskDtoValidator()
+    {
+        RuleFor(x => x.Title)
+            .NotEmpty().WithMessage("Title is mandatory")
+            .MaximumLength(50);
+
+        RuleFor(x => x.Description)
+            .NotEmpty().WithMessage("Description is mandatory")
+            .MaximumLength(200);
+
+        RuleFor(x => x.UserAssignedId)
+            .NotEmpty().WithMessage("Assigned user is mandatory");
+    }
+}

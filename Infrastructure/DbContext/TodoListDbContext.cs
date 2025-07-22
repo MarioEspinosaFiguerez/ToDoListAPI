8dff690dac9f07fcf76c7026516e27a26b758630
Mario Espinosa <79775251+MarioEspinosaFiguerez@users.noreply.github.com>
2025-07-22 03:42:32 +0200

    DbContext without Guid as autoIncremental (ValueGenerated)

---
+++ Infrastructure/DbContext/TodoListDbContext.cs
@@ -0,0 +1,24 @@
+﻿namespace Infrastructure.AppDbContext;
+
+public class TodoListDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<ToDoTask> Tasks { get; set; }
+
+    public TodoListDbContext(DbContextOptions<TodoListDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // User
+        modelBuilder.Entity<User>().Property(u => u.Id).ValueGeneratedNever();
+
+        // Task
+        modelBuilder.Entity<ToDoTask>().Property(task => task.Id).ValueGeneratedNever();
+
+        // Relations
+        modelBuilder.Entity<ToDoTask>().HasOne(task => task.AssignedTo).WithMany().HasForeignKey(task => task.UserAssignedId).IsRequired();
+
+
+        base.OnModelCreating(modelBuilder);
+    }
+}

845871cdd0cee84f109e1e138d49afdc9269ade7
Mario Espinosa <79775251+MarioEspinosaFiguerez@users.noreply.github.com>
2025-07-22 21:27:27 +0200

    Added record DTO for TodoTask

---
+++ Application/DTOs/ToDoTaskDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.DTOs;
+
+public record ToDoTaskDTO
+{
+    public string Title { get; init; } = string.Empty;
+    public string Description { get; init; } = string.Empty;
+    public Guid UserAssignedId { get; init; }
+    public Priority Priority { get; init; }
+    public State State { get; init; } = State.Pending;
+    public DateTimeOffset Deadline { get; init; }
+}

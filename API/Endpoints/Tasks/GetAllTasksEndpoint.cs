9d27dda39dba3db8a548821641e36f53c1e23986
Mario Espinosa <79775251+MarioEspinosaFiguerez@users.noreply.github.com>
2025-07-23 13:34:24 +0200

    GetAlLTaskEndpoint added OpenApi info

--- API/Endpoints/Tasks/GetAllTasksEndpoint.cs
+++ API/Endpoints/Tasks/GetAllTasksEndpoint.cs
@@ -5,22 +5,25 @@ public class GetAllTasksEndpoint : IEndpoint
 {
     public void MapEndpoint(IEndpointRouteBuilder app)
     {
-        // Assign Multiples routes using the same Handler
-        async Task<IResult> GetAllTasks(ITaskService _taskService)
-        {
-            var tasks = await _taskService.GetAllTasks();
-            return Results.Ok(tasks);
-        }
-
-        app.MapGet("/tasks", GetAllTasks).WithTags("Tasks");
-        app.MapGet("/tasks/getAllTasks", GetAllTasks).WithTags("Tasks");
-
-        /* Using 1 Route for 1 Handler
         app.MapGet("/tasks", async (ITaskService _taskService) =>
         {
             var tasks = await _taskService.GetAllTasks();
             return Results.Ok(tasks);
-        });
-        */
+        })
+        .WithName("GetAllTasks")
+        .WithTags("Tasks")
+        .Produces<IEnumerable<ToDoTaskDTO>>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status500InternalServerError)
+        .WithOpenApi(opt =>
+        {
+            opt.Summary = "List all tasks";
+            opt.Responses = new Microsoft.OpenApi.Models.OpenApiResponses
+            {
+                ["200"] = new Microsoft.OpenApi.Models.OpenApiResponse { Description = "Sucessfull operation. Return the list of tasks, empty if there's no tasks" },
+                ["500"] = new Microsoft.OpenApi.Models.OpenApiResponse { Description = "Internal server error" }
+            };
+
+            return opt;
+        });       
     }
 }

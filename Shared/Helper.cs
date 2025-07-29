f758ae5f582991bc86114b12bb838519e8e81da7
Mario Espinosa <79775251+MarioEspinosaFiguerez@users.noreply.github.com>
2025-07-29 11:33:02 +0200

    Added Shared project for helper classes

---
+++ Shared/Helper.cs
@@ -0,0 +1,7 @@
+﻿namespace Shared;
+
+public static class Helper
+{
+    public static string MergeIntoFullName(string name, string firstSurname, string? secondSurname) =>
+    string.IsNullOrEmpty(secondSurname) ? $"{name} {firstSurname}" : $"{name} {firstSurname} {secondSurname}";
+}

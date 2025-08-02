4059fe9bc76d8216e9be9fa21a51912b97a53b07
Mario Espinosa <79775251+MarioEspinosaFiguerez@users.noreply.github.com>
2025-08-02 11:54:14 +0200

    Quit comments

--- Infrastructure/Repositories/UserRepository.cs
+++ Infrastructure/Repositories/UserRepository.cs
@@ -8,14 +8,12 @@ public class UserRepository : IUserRepository
 
     public async Task<bool> ExistsAsync(string email) => await _context.Users.AnyAsync(user => user.Email == email);
 
-    // IQueryable doesn't execute the query, only create it. It executes when doing the ToList / First() / CountAsync / AnyAsync()
     public IQueryable<User> GetAllUsers() => _context.Users.AsNoTracking();
 
     public async Task<User?> GetUserById(Guid id) => await _context.Users.FindAsync(id);
 
     public async Task<User> CreateUser(User request)
     {
-        // EF Core automatically creates a Transaction and make a Rollback if error is thrown
         await _context.Users.AddAsync(request);
         await _context.SaveChangesAsync();
 
@@ -24,7 +22,6 @@ public class UserRepository : IUserRepository
 
     public async Task<User> UpdateUser(User request)
     {
-        // Tracked auto because of the tracking in the service with the GetUserById() - FindAsync tracked the Entity to EF Core
         await _context.SaveChangesAsync();
 
         return request;

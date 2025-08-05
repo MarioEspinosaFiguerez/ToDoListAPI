8ce67af2f7d82b6250b79c575da22ecef5676e3b
Mario Espinosa <79775251+MarioEspinosaFiguerez@users.noreply.github.com>
2025-08-05 12:42:22 +0200

    Task Validator

--- Application/Validators/TaskValidators.cs
+++ Application/Validators/TaskValidators.cs
@@ -12,26 +12,50 @@ public class CreateToDoTaskDTOValidator : AbstractValidator<CreateToDoTaskReques
             .NotEmpty().WithMessage("Description is mandatory")
             .MaximumLength(200);
 
-        /*RuleFor(x => x.UserAssignedId)
+        RuleFor(x => x.UserAssignedId)
             .NotEmpty().WithMessage("Assigned user is mandatory")
-            .MustAsync(async (userId, cancellationToken) => await _userService.ExistsAsync(userId))
-            .WithMessage(task => $"User with Id '{task.UserAssignedId}' doesn't exist.");*/
+            .MustAsync(async (userId, cancellationToken) => await _userService.ExistsAsyncById(userId) )
+            .WithMessage(task => $"User with Id '{task.UserAssignedId}' doesn't exist.");
 
-        RuleFor(x => x.Priority.Id)
-            .GreaterThan(0).WithMessage("Priority must be one of the following values: Low (1), Medium (2), High (3).");
+        RuleFor(x => x.PriorityId)
+            .InclusiveBetween(1, 3).WithMessage("PriorityId must be one of the following values: Low (1), Medium (2), High (3).");
 
         RuleFor(x => x.Deadline)
-            .GreaterThan(DateTimeOffset.Now).WithMessage("Deadline must be a future date");
+            .Cascade(CascadeMode.Stop) // If the first rule is false then the second one doesn't execute
+            .NotEmpty().WithMessage("Deadline is mandatory")
+            .Must(dateStr => DateTimeOffset.TryParse(dateStr, out var date) && date > DateTimeOffset.Now)
+            .WithMessage("Deadline must be a future date");
     }
 }
-
-public class ToDoTaskDTOValidator : AbstractValidator<ToDoTaskDTO>
+public class UpdateToDoTaskDTOValidator : AbstractValidator<UpdateToDoTaskRequest>
 {
-    public ToDoTaskDTOValidator(IUserService _userService)
+    public UpdateToDoTaskDTOValidator(IUserService _userService)
     {
-        Include(new CreateToDoTaskDTOValidator(_userService));
+        RuleFor(x => x.Title)
+           .MaximumLength(50);
 
-        RuleFor(x => x.State.Id)
-           .GreaterThan(0).WithMessage("State must be one of the following values: Pending (1), InProgress (2), Completed (3).");
+        RuleFor(x => x.Description)
+            .MaximumLength(200);
+
+        RuleFor(x => x.UserAssignedId)
+            .MustAsync(async (userId, cancellationToken) => 
+             userId.HasValue &&  await _userService.ExistsAsyncById(userId.Value))
+            .When(x => x.UserAssignedId is not null)
+            .WithMessage(task => $"User with Id '{task.UserAssignedId}' doesn't exist.");
+
+        RuleFor(x => x.PriorityId)
+            .InclusiveBetween(1, 3)
+            .When(x => x.PriorityId.HasValue)
+            .WithMessage("PriorityId must be one of the following values: Low (1), Medium (2), High (3).");
+
+        RuleFor(x => x.StateId)
+           .InclusiveBetween(1, 3)
+           .When(x => x.PriorityId.HasValue)
+           .WithMessage("State must be one of the following values: Pending (1), InProgress (2), Completed (3).");
+
+        RuleFor(x => x.Deadline)
+            .Must(dateStr => DateTimeOffset.TryParse(dateStr, out var date) && date > DateTimeOffset.Now)
+            .When(x => !string.IsNullOrEmpty(x.Deadline))
+            .WithMessage("Deadline must be a future date");
     }
 }

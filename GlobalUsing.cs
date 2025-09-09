94ec0d3a6349824a520951eeb451c078e86f590a
Mario Espinosa <79775251+MarioEspinosaFiguerez@users.noreply.github.com>
2025-09-09 13:26:25 +0200

    Added new Global usings

--- GlobalUsing.cs
+++ GlobalUsing.cs
@@ -1,11 +1,11 @@
 ﻿global using System;
 global using System.Collections.Generic;
 global using System.Data.Common;
+global using System.Globalization;
 global using System.Linq;
 global using System.Net;
+global using System.Reflection;
 global using System.Text;
 global using System.Text.Json;
 global using System.Threading;
 global using System.Threading.Tasks;
-global using System.Globalization;
-global using System.Reflection;
